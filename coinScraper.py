284d051bb1aa211126ce46430bb531df0d536cf6
mudonja <aljkavi007@gmail.com>
2020-07-20 15:19:23 -0400

    write all Tags and Links to a file - Veljko

--- coinScraper.py
+++ coinScraper.py
@@ -4,49 +4,31 @@ import requests
 import urllib.request
 import time
 from bs4 import BeautifulSoup
+# TODO: Implement torrequests so user can use TOR
 # from torrequest import TorRequest
 import os
 import re
 import time
+import json
 
-
-
+# Used for github credentials
 #usrname = input("Github username:")
 #passwrd = input("Github password:")
-'''
-#Section that tests that IP was different using TOR
-response=requests.get('http://ipecho.net/plain')
-print("My Original IP Address:", response.text,"\n")
-for i in range(4):
-	with TorRequest(password='CC45B57A1F3D2DF860B4352CBD1F0233CA9A9DACA83202E88BB263A6B1') as tr:
-		tr.reset_identity() #Reset TOR
-		response=tr.get('http://ipecho.net/plain')
-		print("New IP Address",response.text)
-	time.sleep(5)
-'''
-# with TorRequest(password='CC45B57A1F3D2DF860B4352CBD1F0233CA9A9DACA83202E88BB263A6B1') as tr:
-#	tr.reset_identity() #Reset TOR
-# Set the URL you want to webscrape from
-### provide an input argument
-base_url = 'https://coinmarketcap.com'
-#############TODO###########git_base_url = "https://" + usrname + ":" + passwrd + "@github.com"
-# Connect to the URL
-###response = requests.get(base_url + "/all/views/all/")
-# response = tr.get(base_url)
-# Parse HTML and save to BeautifulSoup object
-#soup = BeautifulSoup(response.text, "html.parser")
-###soup = BeautifulSoup(response.content, 'lxml').find_all('a', attrs={"class": ["cmc-link"]}, href=True,  )
-#currency_link = soup.find_all('a', href=True) #(class_="currency-name-container link-secondary")
 
-#print(source_code.status_code)
-# Load all cryptocurrency links
+
+base_url = 'https://coinmarketcap.com'
+# Line bellow is important
+# TODO Uncomment following
+# git_base_url = "https://" + usrname + ":" + passwrd + "@github.com"
+
+# Load all cryptocurrency relative paths to form cryptocurrency links on Coin Market Cap
 i=1
 page_num=1
 currency_pages=dict()
-'''
+
 while(True):
-    #napTime = random.randint(5, 10)
-    #time.sleep(napTime)
+    napTime = random.randint(5, 10)
+    time.sleep(napTime)
     source_code = requests.get(base_url+"/"+str(page_num)+"/")
     if(source_code.status_code == 200):
         plain_text = source_code.content
@@ -55,31 +37,31 @@ while(True):
         for link in links:
             if(link.get('title')!=None and "/currencies/" in str(link.get('href'))):
                 print(i,link.get('title'),"\n",link.get('href'))
-                currency_pages[link.get('title').tolower]=link.get('href')
+                currency_pages[link.get('title')]=link.get('href')
                 i+=1
         page_num+=1
+        break
     elif(source_code.status_code==404):
         print("Page not found:",source_code.status_code)
         break
     else:
         print("Code:",source_code.status_code)
-'''
-#for key,val in currency_pages.items():
-#    print(key,":",val,"\n")
 
 # TODO: coin gecko has an API to get all tickers with id so use id to access currency and strip data
 Download_counter = 1
 total = 1
 start_time = time.time()
-# TODO:  Go to each one, and access currency and strip all data(tags)
-#    then clone the repo
 #currency_pages- hold all names and rel paths for every cryptocurrency
-currency_pages={"Bitcoin":"/currencies/bitcoin/","Tether":"/currencies/tether/"}
+#currency_pages={"Bitcoin":"/currencies/bitcoin/","Tether":"/currencies/tether/"}  # Used for testing
 tags=dict()  # Store tags for a cryptocurrency(key is currency name eg bitcoin)
 currency_github=dict()  # Store github links for each crypto currency(key is currency name eg bitcoin)
+
+# This for loop scrapes SourceCode link and Tags
 for key,val in currency_pages.items():
     #key=name
     #val=relative path
+    napTime = random.randint(5, 10)
+    time.sleep(napTime)
     response = requests.get(base_url + val)
     if (response.status_code == 200):
         plain_text = response.content
@@ -93,11 +75,24 @@ for key,val in currency_pages.items():
         links = soup.findAll('a', string="Source Code", href=True)
         for link in links:
             currency_github[key] = link.get('href')
-        print(tags,'\n',currency_github)
+
+        #print(tags,'\n',currency_github)
 # TODO: clone git repos using currency_github for links
     else:
-        print("something is wrong")
+        print("Response did not return status 200 OK","\n","Response status:",response.status_code)
+with open("./codeSimTags.txt",'a+') as f:
+    f.write(json.dumps(tags))
+    f.close()
+with open("./codeSimLinks.txt",'a+') as f:
+    f.write(json.dumps(currency_github))
+    f.close()
+
+
+
+
+
 '''
+
     if len(curr_git) > 1:
         # resp=tr.get(str(curr_git[1]))
         resp = requests.get(str(curr_git[1]))
